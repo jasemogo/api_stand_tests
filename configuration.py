6592b7009e8864cf7450376b6ad1283076de65ae
Jaime Sebastian Moreno <j.sebastian.morenogonzalez@gmail.com>
2024-09-09 14:12:48 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+
+URL_SERVICE = "https://cnt-85ac5622-da9d-4907-962a-90691d888286.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
